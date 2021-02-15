3ae9e91fff157c17fc08c00fdf3f29179c25d6f3
南漂一卒 <shiy2008@gmail.com>
2021-02-16 02:38:40 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import { auto } from 'browser-unhandled-rejection';
+import { serviceWorkerUpdate } from 'web-utility';
+import { documentReady, render, createCell } from 'web-cell';
+
+import { PageFrame } from './page';
+
+auto();
+
+self.addEventListener('unhandledrejection', ({ reason }) => {
+    const { message } = reason as Error;
+
+    if (message) self.alert(message);
+});
+
+const { serviceWorker } = window.navigator;
+
+if (process.env.NODE_ENV !== 'development')
+    serviceWorker
+        ?.register('sw.js')
+        .then(serviceWorkerUpdate)
+        .then(worker => {
+            if (
+                window.confirm(
+                    'New version of this Web App detected, update now?'
+                )
+            )
+                worker.postMessage({ type: 'SKIP_WAITING' });
+        });
+
+serviceWorker?.addEventListener('controllerchange', () =>
+    window.location.reload()
+);
+
+documentReady.then(() => render(<PageFrame />));
