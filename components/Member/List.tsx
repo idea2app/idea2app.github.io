def00d6e26c03b54a8c8306a26c7610228772809
Leo Wang <leo@wangrunlin.com>
2023-02-25 06:54:55 +0800

    [optimize] update components/Member/List.tsx

--- components/Member/List.tsx
+++ components/Member/List.tsx
@@ -10,7 +10,7 @@ import { MemberCard } from './Card';
 export const MemberListLayout: FC<{ defaultData: Member[] }> = ({
   defaultData,
 }) => (
-  <Row as="ul" className="list-unstyled g-4" xs={1} sm={2} md={3}>
+  <Row as="ul" className="list-unstyled g-4" xs={1} md={2} lg={3}>
     {defaultData.map(item => (
       <Col as="li" key={item.id + ''}>
         <MemberCard className="h-100 shadow-sm" {...item} />
