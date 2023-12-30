5a56924c425b6b79fa3b6ced5020cf74d38f6b65
TechQuery <shiy2008@gmail.com>
2023-12-30 20:23:21 +0800

    [add] Sentry monitor configuration [optimize] Resource Limitation of Docker Compose [optimize] update Upstream packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
-import webpack from 'webpack';
-import withLess from 'next-with-less';
+import { withSentryConfig } from '@sentry/nextjs';
 import setPWA from 'next-pwa';
+import withLess from 'next-with-less';
+import webpack from 'webpack';
 
-const { NODE_ENV } = process.env,
-  withPWA = setPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: NODE_ENV === 'development',
-  });
+const { NODE_ENV, SENTRY_AUTH_TOKEN, SENTRY_ORG, SENTRY_PROJECT } = process.env;
+const isDev = NODE_ENV === 'development';
+
+const withPWA = setPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: isDev,
+});
 
 /**
  * @type {import('next').NextConfig}
  */
-export default withPWA(
+const nextConfig = withPWA(
   withLess({
     webpack: config => {
       config.plugins.push(
@@ -25,3 +28,21 @@ export default withPWA(
     },
   }),
 );
+
+export default isDev || !SENTRY_AUTH_TOKEN
+  ? nextConfig
+  : withSentryConfig(
+      {
+        ...nextConfig,
+        sentry: {
+          transpileClientSDK: true,
+          autoInstrumentServerFunctions: false,
+        },
+      },
+      {
+        org: SENTRY_ORG,
+        project: SENTRY_PROJECT,
+        authToken: SENTRY_AUTH_TOKEN,
+        silent: true,
+      },
+    );
